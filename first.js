dc00a48397b577a1440289a9b53ddf05c144ed2d
alemughal <alimug38@gmail.com>
2021-08-04 20:57:27 +0500

    First TypeScript Code

---
+++ first.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = 169;
+var sum = function (a, b) { return a + b; };
+sum(10, 20);
+var greetings = "Hello World!";
+console.log(greetings);
---
+++ first.ts
@@ -0,0 +1,12 @@
+var a:number = 169;
+
+const sum = (a:number,b:number):number => a + b;
+
+sum(10,20);
+
+var greetings = "Hello World!"
+
+console.log(greetings);
+
+
+export {};
