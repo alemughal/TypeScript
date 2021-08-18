07220b64c39bb32452f4cf8c67f4a926a4651ac3
alemughal <alimug38@gmail.com>
2021-08-18 14:07:35 +0500

    Setting up webpack

--- first.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var a = 169;
-var sum = function (a, b) { return a + b; };
-sum(10, 20);
-var greetings = "Hello World!";
-console.log(greetings);
